3260bedf857dc53c566f3539c88d8a8086c08a78
Moses-Maina-ctrl <mosesmaina1245@gmail.com>
2023-11-03 00:27:02 +0300

    Add button functions

---
+++ script.js
@@ -0,0 +1,33 @@
+
+const btn = document.querySelectorAll("#btn");
+
+
+function checkExpression(exp){
+    if ((parseInt(exp) == exp) || (exp ==".")){
+        console.log(exp);
+    }
+    else if (exp == "+"){
+        console.log("add");
+    }
+    else if (exp == "-"){
+        console.log("minus");
+    }
+    else if (exp == "/"){
+        console.log("divide");
+    }
+    else if (exp =="x"){
+        console.log("multiply");
+    }
+    else if (exp =="="){
+        console.log("equals");
+        }
+}
+btn.forEach(button => button.addEventListener("click", () =>{
+    let expression = button.textContent;
+    checkExpression(expression);
+}))
+
+function joinNumbers(operand){
+    
+
+}
