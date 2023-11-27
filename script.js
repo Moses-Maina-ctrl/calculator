f94b66661ce30228867fe79360dbfcff6f1a3d16
Moses-Maina-ctrl <mosesmaina1245@gmail.com>
2023-11-27 18:09:40 +0300

    Add display results function

--- script.js
+++ script.js
@@ -1,43 +1,83 @@
 
-const btn = document.querySelectorAll("#btn");
-const operands =document.querySelector("#operand > p")
+const equation =document.querySelector("#equation > p")
+const resultScreen =document.querySelector("#answer > p")
+const numbers = document.querySelectorAll('[data-number]')
+const operator = document.querySelectorAll('[data-operator')
+const equals = document.querySelectorAll('#equals')
+const point = document.querySelectorAll('#point')
+const clearBtn = document.querySelectorAll('#clear');
 
-        
-function checkExpression(exp){
-    if ((parseInt(exp) == exp) || (exp ==".")){
-        joinNumbers(exp);
+let value1 = '';
+let value2 = '';
+let sign ='';
+numbers.forEach(button => button.addEventListener("click", () =>{
+    if (value1 != ''){
+        clearResultforSecondValue()
     }
-    else if (exp == "+"){
-        console.log("+");
-    }
-    else if (exp == "-"){
-        console.log("-");
-    }
-    else if (exp == "/"){
-        console.log("/");
-    }
-    else if (exp =="x"){
-        console.log("*");
-        
-    }
-    else if (exp =="="){
-        console.log("equals");
-        }
-    
-}
-btn.forEach(button => button.addEventListener("click", () =>{
-    let expression = button.textContent;
-    checkExpression(expression);
+    joinNumbers(button.textContent)
 }))
-
+clearBtn.forEach(button => button.addEventListener("click", () =>{
+    clear()
+}))
+operator.forEach(button => button.addEventListener("click", () =>{
+    let symbol = button.textContent;
+    if (symbol == 'x'){
+        sign ='*';
+        firstOperand();
+        displayFirstOperand(symbol);  
+    }else if (symbol == '-'){
+        sign = '-'
+        firstOperand();
+        displayFirstOperand(symbol);
+    }else if (symbol == '+'){
+        sign = '+'
+        firstOperand();
+        displayFirstOperand(symbol);
+    }else{
+        sign = '/'
+        firstOperand();
+        displayFirstOperand(symbol);
+    }
+}))
 function joinNumbers(operand){
-    let value = operands.innerText;
-    value = value + operand;
-    operands.innerText =value;
-    
+    resultScreen.textContent += operand   
+} 
+function clearResultforSecondValue(){
+    resultScreen.textContent = '';
+}  
+function displayFirstOperand(a){
+    equation.textContent += ' ' + String(resultScreen.textContent) + ' ' + String(a);
+   
+     
+}
+function firstOperand(){
+    value1 = parseFloat(resultScreen.textContent)
+    return value1
+}
+function clear(){
+    equation.textContent='';
+    resultScreen.textContent='';
+    value1='';
+    value2='';
 }
 
-function clear(exp){
-    let value = ''
-    operands.innerText =value;
+function add(val1,val2){
+    result = val1 + val2;
+}
+
+function subtract(val1,val2){
+    result = val2 -  val2;
+}
+
+
+function multiply(val1,val2){
+    result = val2 * val2;
+}
+
+function multiply(val1,val2){
+    result = val2 * val2;
+}
+
+function operate(){
+
 }
